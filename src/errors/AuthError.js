395b5bf95c93bce30186806060aa6409bd19bf6c
Saeesh Tendulkar <saeesh.tendulkar@gmail.com>
2023-10-17 19:30:27 +0530

    renamed files with class names

---
+++ src/errors/AuthError.js
@@ -0,0 +1,8 @@
+class AuthError extends Error{
+    constructor(message, statusCode){
+        super(message)
+        this.statusCode = statusCode
+    }
+}
+
+export default AuthError
---
+++ src/models/User.js
@@ -0,0 +1,165 @@
+import APIError from "../errors/APIError.js";
+import BaseModel from "../helpers/BaseModel.js";
+ 
+class User extends BaseModel {
+	constructor() {
+		super();
+		(this.database.table = "users"),
+			(this.database.fields = {
+				first_name: {
+					type: "VARCHAR(255)",
+					insert: true,
+				},
+				last_name: {
+					type: "VARCHAR(255)",
+					insert: true,
+				},
+				firebase_id: {
+					type: "VARCHAR(255)",
+					allowNull: false,
+					insert: true,
+					unique: true,
+					validate: {
+						notEmpty: true,
+					},
+				},
+				email: {
+					type: "VARCHAR(255)",
+					allowNull: false,
+					insert: true,
+					unique: true,
+					validate: {
+						notEmpty: true,
+					},
+				},
+				is_active: {
+					type: "TINY_INT(1)",
+					allowNull: false,
+					defaultValue: true,
+				},
+				first_login: {
+					type: "DATETIME",
+				},
+				last_login: {
+					type: "DATETIME",
+				},
+			});
+		this.database.relations = {
+			role_id: {
+				type: "belongsTo",
+			},
+		};
+	}
+
+	static async role() {
+		//return $this->belongsTo(User::class, 'owner_id', 'id');
+	}
+
+	static getFields({ type = false, variable = false }) {
+		let fieldStr = "";
+		for (const key in this.database.fields) {
+			if (this.database.fields[key].insert == true) {
+				if (fieldStr != "") {
+					fieldStr += ",";
+				}
+				field = variable ? "p_" + key : key;
+				fieldStr += field;
+				if (type) {
+					fieldStr += " " + this.database.fields[key].type;
+				}
+			}
+		}
+		return fieldStr;
+	}
+
+	static async createProcedures() {
+		const insertProcedure = `
+        DELIMITER //
+        DROP PROCEDURE IF EXISTS addUser;
+        CREATE PROCEDURE addUser(${this.getFields({
+			type: true,
+			variable: true,
+		})})
+        BEGIN 
+            IF NOT EXISTS (SELECT 'X' FROM ${
+				this.table
+			} WHERE firebase_id = firebase_id) THEN 
+            BEGIN
+                INSERT INTO ${
+					this.database.table
+				}(${this.getFields()}) VALUES (${this.getFields({
+			variable: true,
+		})});
+                SELECT 200 as statusCode, 'success' as message;
+            END
+            ELSE
+            BEGIN 
+                SELECT 403 as statusCode, 'User already exists' as message;
+            END
+            ENDIF
+        END//
+        DELIMITER ;        
+        `;
+		await this.runSQL(insertProcedure);
+	}
+
+	async create({ email, first_name, last_name, role_id, firebase_id }) {
+		const status = await this.callProcedure("addUser", [
+			first_name,
+			last_name,
+			firebase_id,
+			email,
+			role_id,
+		]);
+		if (!status) {
+			throw new APIError(
+				"User creation has failed. Please try again",
+				400
+			);
+		}
+		return status;
+	}
+
+	async update({
+		id,
+		email = null,
+		first_name = null,
+		last_name = null,
+		role_id = null,
+		firebase_id = null,
+		first_login = null,
+		last_login = null,
+		is_active = null,
+	}) {
+		const status = await this.callProcedure("updateUser", [
+			id,
+			first_name,
+			last_name,
+			firebase_id,
+			email,
+			role_id,
+			first_login,
+			last_login,
+			is_active,
+		]);
+		if (!status) {
+			throw new APIError(
+				"User updation has failed. Please try again",
+				400
+			);
+		}
+		return status;
+	}
+
+	async save() {
+		const newProperties = {};
+		for (const key in this) {
+			if (key !== "database" && this.hasOwnProperty(key)) {
+				newProperties[key] = this[key];
+			}
+		}
+		return this.update(newProperties);
+	}
+}
+
+export default new User();
