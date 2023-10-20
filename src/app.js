68c35561a89c548d135dbbf99cb559ff42c89e56
Saeesh Tendulkar <saeesh.tendulkar@gmail.com>
2023-10-20 17:43:21 +0530

    cors filter method added

--- src/app.js
+++ src/app.js
@@ -1,38 +1,32 @@
-import express from "express"
-import cors from "cors"
-import "dotenv/config"
-import userRoutes from "./routes/user.js"
-import notFound from "./middleware/notFound.js"
-import errorHandler from "./middleware/errorHandler.js"
-import cookieParser from "cookie-parser"
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import userRoutes from "./routes/user.js";
+import notFound from "./middleware/notFound.js";
+import errorHandler from "./middleware/errorHandler.js";
+import cookieParser from "cookie-parser";
 
-const PORT = process.env.PORT || 3000
-const app = express()
+const PORT = process.env.PORT || 3000;
+const app = express();
 
-const whitelist = JSON.parse(process.env.CORS_WHITELIST)
-const corsOptions = {
-	origin: function (origin, callback) {
-		if (!origin) {
-			return callback(null, true)
-		}
-		if (whitelist.indexof(origin) !== -1) {
-			callback(null, true)
-		} else {
-			callback(new Error("Not allowed by CORS"))
-		}
-	},
-}
-app.use(cors({
-    origin: '*'
-}));
-//app.use(cors(corsOptions))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cookieParser())
-app.use("/user", userRoutes)
-app.use(notFound)
-app.use(errorHandler)
+const whitelist = JSON.parse(process.env.CORS_WHITELIST);
+const corsOptions = function (req, callback) {
+	var corsOptions;
+	if (whitelist.indexOf(req.header("Origin")) !== -1) {
+		corsOptions = { origin: true };
+	} else {
+		corsOptions = { origin: false };
+	}
+	callback(null, corsOptions);
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use("/user", userRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`)
-})
+	console.log(`Server is running on port ${PORT}`);
+});
