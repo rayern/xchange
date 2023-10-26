91f7036ebaea1875d48e5560618ca1668a789acc
Saeesh Tendulkar <saeesh.tendulkar@gmail.com>
2023-10-26 07:34:31 +0530

    cors credentials added

--- src/app.js
+++ src/app.js
@@ -5,27 +5,54 @@ import userRoutes from "./routes/user.js";
 import notFound from "./middleware/notFound.js";
 import errorHandler from "./middleware/errorHandler.js";
 import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 const whitelist = JSON.parse(process.env.CORS_WHITELIST);
 const corsOptions = function (req, callback) {
-	var corsOptions;
+	var corsOptions = {
+		methods: ["GET", "POST"],
+		credentials: true,
+	};
 	if (whitelist.indexOf(req.header("Origin")) !== -1) {
-		corsOptions = { origin: true };
+		corsOptions.origin = true;
 	} else {
-		corsOptions = { origin: false };
+		corsOptions.origin = false;
 	}
 	callback(null, corsOptions);
 };
-//app.use(cors(corsOptions));
-app.use(cors({
-    origin: '*'
-}));
+// app.use(cors({
+// 	origin: true,
+// 	methods: ["GET", "POST"],
+// 	credentials: true
+// }));
+
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+		credentials: true,
+	})
+);
+
+app.use(function (req, res, next) {
+	res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, OPTIONS, PUT, PATCH, DELETE"
+	);
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"X-Requested-With,content-type"
+	);
+	res.setHeader("Access-Control-Allow-Credentials", true);
+	next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(bodyParser.json());
 app.use("/user", userRoutes);
 app.use(notFound);
 app.use(errorHandler);
