54d1ca47c6a3b7bc0e76f3028b1de00119ef6cde
Sambit Basu <sambit@bixan.com>
2023-11-27 08:51:32 -0800

    WIP

--- src/controllers/itemController.js
+++ src/controllers/itemController.js
@@ -5,11 +5,17 @@ import AuthErrorfrom from "../errors/authError.js";
 import {getAllItems, handleNewItem} from "../service/itemService.js";
 
 export const getAll = async (req, res) => {
-    let items = await getAllItems(req.params.startIdx);
-    
-    return res
-        .status(200)
-        .json({success: true, data: items[0], message: ""})
+    await getAllItems(req.params.startIdx)
+        .then((data) => {
+            return res
+                .status(200)
+                .json({success: true, data: data[0], message: ""});
+        })
+        .catch((err) => {
+            return res
+                .status(500)
+                .json({success: false, message: err});
+        })
 };
 
 export const addNew = async (req, res) => {
